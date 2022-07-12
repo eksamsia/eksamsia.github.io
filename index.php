cd587a152566d98b954b6d1d586e166c33e0edcf
eksamsia <ekasamsiati03@mail.ugm.ac.id>
2022-07-12 11:43:41 +0700

    landing page

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simpelmasjar landing page</title>
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header class="showcase">
+        <div class="showcase-top">
+            <a href="#" class="showcase-top-1">register</a>
+            <a href="https://simpelmasjar.nganjukkab.go.id/login
+" class="btn btn-rounded">Sign In</a>
+
+        </div>
+        <div class="showcase-content">
+            <div class="sbuttons">
+
+                <a href="mailto:kesbangpollinmas@nganjukkab.go.id" target="_blank" class="sbutton email"
+                    tooltip="Email"><i class="fa-solid fa-envelope"></i></a>
+
+                <a href="tel:(0358)328079" target="_blank" class="sbutton phone" tooltip="Telepon (0358) 328079"><i
+                        class="fa-solid fa-phone"></i></i></a>
+
+                <a target="_blank" class="sbutton mainsbutton" tooltip="Info lebih lanjut"><i
+                        class="fa-solid fa-question"></i></a>
+
+            </div>
+            <p><b>Perizinan</b></p>
+            <!-- <p>Perizinan Rekomendasi Penelitian</p> -->
+            <p><b>Rekomendasi Penelitian</b></p>
+            <!-- <h2>sebelum download, harap membaca persyaratan di bawah ini</h2> -->
+            <br>
+            <a href="https://simpelmasjar.nganjukkab.go.id/file/FormPenelitian.doc" class="btn btn-xl">Download Formulir
+                Persyaratan</a>
+            <br>
+            <h6>*sebelum download, harap membaca persyaratan di bawah ini</h6>
+        </div>
+    </header>
+    <section class="tabs">
+        <div class="container">
+            <div id="tab-1" class="tab-item ">
+                <!-- <i class="fas fa-door-open fa-3x"></i> -->
+                <p class="hide-sm">Dasar Hukum</p>
+            </div>
+            <div id="tab-2" class="tab-item">
+                <!-- <i class="fas fa-tablet-alt fa-3x"></i> -->
+                <p class="hide-sm">Tujuan</p>
+            </div>
+            <div id="tab-3" class="tab-item">
+                <!-- <i class="fas fa-tags fa-3x"></i> -->
+                <p class="hide-sm">Ketentuan</p>
+            </div>
+            <div id="tab-4" class="tab-item tab-border">
+                <!-- <i class="fas fa-door-open fa-4x"></i> -->
+                <p class="hide-sm">Kategori Peneliti</p>
+            </div>
+            <!-- <div id="tab-5" class="tab-item">
+                <i class="fas fa-tablet-alt fa-3x"></i>
+                <p class="hide-sm">Lembaga / PT</p>
+            </div> -->
+            <div id="tab-6" class="tab-item">
+                <!-- <i class="fas fa-tags fa-3x"></i> -->
+                <p class="hide-sm">Catatan</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="tab-content">
+        <div class="container">
+            <!-- Tab Content 1 dasar hukum -->
+            <div id="tab-1-content" class="tab-content-item">
+                <div class="tab-1-content-inner">
+                    <div>
+                        <ol>
+                            <li>Undang-undang Nomor 18 Tahun 2002 tentang Sistem Nasional Penelitian, Pengembangan,
+                                dan Penerapan Ilmu Pengetahuan dan Teknologi</li> <br>
+                            <li>Peraturan Menteri Dalam Negeri Nomor 20 Tahun 2011 tentang Pedoman Penelitian dan
+                                Pengembangan di Lingkungan Kementerian Dalam Negeri dan Pemerintah Daerah</li> <br>
+                            <li>Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 7 Tahun 2014 tentang
+                                Perubahan Atas Peraturan Menteri Dalam Negeri Republik Indonesia Nomor 64 Tahun 2011
+                                tentang Pedoman Penertiban Rekomendasi Penelitian</li>
+                        </ol>
+                    </div>
+                    <!-- <img src="https://i.ibb.co/J2xDJV7/tab-content-1.png" alt="" /> -->
+                </div>
+            </div>
+
+            <!-- Tab Content 2 tujuan-->
+            <div id="tab-2-content" class="tab-content-item">
+                <div class="tab-1-content-top">
+                    <div>
+                        <ol>
+                            <li>
+                                Bahan Pertimbangan Pemberian Izin Penelitian oleh Pemerintah Daerah
+                            </li><br>
+                            <li>
+                                Acuan Bagi Peneliti dalam Memperoleh Izin Penelitian
+                            </li>
+                        </ol>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Tab Content 3 ketentuan -->
+            <div id="tab-3-content" class="tab-content-item">
+                <div class="tab-1-content-top">
+                    <ol>
+                        <li>
+                            Masa Berlaku 3 (tiga) bulan untuk S1
+                        </li><br>
+                        <li>
+                            Masa Berlaku 6 (enam) bulan untuk S2 atau Lembaga
+                        </li>
+                    </ol>
+                </div>
+            </div>
+
+            <!-- Tab Content 4 Kategori peneliti-->
+            <div id="tab-4-content" class="tab-content-item show">
+                <div class="text-center">
+                    <p class="text-lg">
+                        Silakan melengkapi dokumen sesuai lokasi universitas / lembaga:
+                    </p>
+                </div>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Jatim</th>
+                            <th>Luar Jatim</th>
+                            <th>Luar Negeri</th>
+                            <th>Lembaga (Jatim)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Pengantar dari Universitas/Lembaga (Asli)</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Proposal (Foto Copy)</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Foto Copy Identitas/ KTP</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Formulir Izin Penelitian yang telah diisi dan dibubuhi materai Rp. 10.000,-</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>-</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Surat dari Bakesbangpol Provinsi Jawa Timur (Asli)</td>
+                            <td>-</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                            <td>✔</td>
+                        </tr>
+                        <tr>
+                            <td>Paspor / Visa (Foto Copy)</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>✔</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td>Surat Keterangan Jalan dari Kepolisian (Foto Copy)</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>✔</td>
+                            <td>-</td>
+                        </tr </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- Tab Content 5 lembaga/pt -->
+        <!-- <div id="tab-5-content" class="tab-content-item">
+            <div class="tab-2-content-top">
+                <div>
+                    <ol>
+                        <li>Surat dari Bakesbangpol Provinsi Jawa Timur (Asli)</li> <br>
+                        <li>Pengantar dari Universitas/Lembaga (Asli)</li> <br>
+                        <li>Proposal (Foto Copy)</li>
+                        <li>Foto Copy Identitas/ KTP</li>
+                        <li>Formulir Izin Penelitian yang telah diisi dan dibubuhi materai Rp. 10.000,-</li>
+                    </ol>
+                </div>
+            </div>
+        </div> -->
+
+        <!-- Tab Content 6 catatan-->
+        <div id="tab-6-content" class="tab-content-item">
+            <div class="tab-1-content-top">
+                <!-- <ol>
+                    <li>Untuk pengambilan surat rekomendasi penelitian, harap membawa dokumen (hardcopy)
+                        asli yang telah dikirim ke email kami sebagai kelengkapan persyaratan pengajuan rekomendasi
+                        penelitian.</li><br>
+                    <li>
+                        Untuk pengisian formulir permohonan rekomendasi penelitian/survey/magang, harap
+                        menggunakan huruf balok untuk menghindari kesalahan dalam entry data
+                    </li>
+                </ol> -->
+                <p class="text-center">Untuk pengambilan surat rekomendasi penelitian, harap membawa dokumen (hardcopy)
+                    asli yang telah dikirim ke email kami sebagai kelengkapan persyaratan pengajuan rekomendasi
+                    penelitian.<br>
+                </p>
+                <p class="text-center">Untuk pengisian formulir permohonan rekomendasi penelitian/survey/magang, harap
+                    menggunakan huruf balok untuk menghindari kesalahan dalam entry data</p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+    <!-- <footer class="footer">
+        <p>Questions? Call 1-866-579-7172</p>
+        <div class="footer-cols">
+            <ul>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Investor Relations</a></li>
+                <li><a href="#">Ways To Watch</a></li>
+                <li><a href="#">Corporate Information</a></li>
+                <li><a href="#">Netflix Originals</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Account</a></li>
+                <li><a href="#">Redeem Gift Cards</a></li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Speed Test</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">Media Center</a></li>
+                <li><a href="#">Buy Gift Cards</a></li>
+                <li><a href="#">Cookie Preferences</a></li>
+                <li><a href="#">Legal Notices</a></li>
+            </ul>
+        </div>
+
+    </footer> -->
+    <script src="app.js"></script>
+</body>
+
+</html>
